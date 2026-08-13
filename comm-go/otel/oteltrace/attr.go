43e3cbccba7a123e9eca97660a21aa0aada975fe
rongwei.liu <568264317@qq.com>
2026-08-13 10:14:21 +0800

    feat(comm-go): accept-language negotiation

--- comm-go/otel/oteltrace/attr.go
+++ comm-go/otel/oteltrace/attr.go
@@ -21,7 +21,7 @@ const (
 	KEY_HTTP_URL                    = "http.url"
 	KEY_HTTP_METHOD                 = "http.method"
 	KEY_HTTP_HEADER_METHOD_OVERRIDE = "http.header.X-Http-Method-Override"
-	KEY_HTTP_HEADER_X_LANGUAGE      = "http.header.X-Language"
+	KEY_HTTP_HEADER_ACCEPT_LANGUAGE = "http.header.Accept-Language"
 	KEY_HTTP_HEADER_CONTENT_TYPE    = "http.header.Content-Type"
 	KEY_HTTP_HEADER_USER_AGENT      = "http.header.User-Agent"
 	KEY_HTTP_HEADER_ACCOUNT_ID      = "http.header.X-Account-ID"
@@ -37,7 +37,7 @@ const (
 	HTTP_HEADER_USER_AGENT      = "User-Agent"
 	HTTP_HEADER_FORWARDED_FOR   = "X-Forwarded-For"
 	HTTP_HEADER_METHOD_OVERRIDE = "X-Http-Method-Override"
-	HTTP_HEADER_X_LANGUAGE      = "X-Language"
+	HTTP_HEADER_ACCEPT_LANGUAGE = "Accept-Language"
 	HTTP_HEADER_ACCOUNT_ID      = "x-account-id"
 	HTTP_HEADER_ACCOUNT_TYPE    = "x-account-type"
 )
@@ -47,7 +47,7 @@ type TraceAttrs struct {
 	HttpUrl            string
 	HttpMethod         string
 	HttpMethodOverride string
-	HttpXLanguage      string
+	HttpAcceptLanguage string
 	HttpContentType    string
 	HttpUserAgent      string
 	HttpAccountID      string
@@ -63,7 +63,7 @@ func GetAttrsByGinCtx(c *gin.Context) TraceAttrs {
 		HttpMethod:         c.Request.Method,
 		HttpContentType:    c.GetHeader(CONTENT_TYPE_NAME),
 		HttpMethodOverride: c.GetHeader(HTTP_HEADER_METHOD_OVERRIDE),
-		HttpXLanguage:      c.GetHeader(HTTP_HEADER_X_LANGUAGE),
+		HttpAcceptLanguage: c.GetHeader(HTTP_HEADER_ACCEPT_LANGUAGE),
 		HttpUserAgent:      c.GetHeader(HTTP_HEADER_USER_AGENT),
 		HttpAccountID:      c.GetHeader(HTTP_HEADER_ACCOUNT_ID),
 		HttpAccountType:    c.GetHeader(HTTP_HEADER_ACCOUNT_TYPE),
@@ -86,7 +86,7 @@ func AddHttpAttrs4API(span trace.Span, attrs TraceAttrs) {
 		attr.Key(KEY_HTTP_URL).String(attrs.HttpUrl),
 		attr.Key(KEY_HTTP_METHOD).String(attrs.HttpMethod),
 		attr.Key(KEY_HTTP_HEADER_CONTENT_TYPE).String(attrs.HttpContentType),
-		attr.Key(KEY_HTTP_HEADER_X_LANGUAGE).String(attrs.HttpXLanguage),
+		attr.Key(KEY_HTTP_HEADER_ACCEPT_LANGUAGE).String(attrs.HttpAcceptLanguage),
 		attr.Key(KEY_HTTP_HEADER_USER_AGENT).String(attrs.HttpUserAgent),
 		attr.Key(KEY_HTTP_HEADER_ACCOUNT_ID).String(attrs.HttpAccountID),
 		attr.Key(KEY_HTTP_HEADER_ACCOUNT_TYPE).String(attrs.HttpAccountType),
